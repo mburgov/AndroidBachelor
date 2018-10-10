0d450c928264361d22679301acdfee253a57741a
Bogdan <ene.bogd@gmail.com>
2018-10-10 11:52:15 +0200

    First Bluetooth Iteration

--- app/src/main/java/dk/bachelor/via/holobachelor/MainActivity.java
+++ app/src/main/java/dk/bachelor/via/holobachelor/MainActivity.java
@@ -1,13 +1,136 @@
 package dk.bachelor.via.holobachelor;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+    BluetoothAdapter mBAdapter;
+    BluetoothManager mBManager;
+    BluetoothLeAdvertiser mBLEAdvertiser;
+    static final int BEACON_ID = 1775;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        mBAdapter = mBManager.getAdapter();
+        mBLEAdvertiser = mBAdapter.getBluetoothLeAdvertiser();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mBAdapter == null || !mBAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBtIntent);
+            finish();
+            return;
+        }
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "No LE support on this device", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        if (!mBAdapter.isMultipleAdvertisementSupported()) {
+            Toast.makeText(this, "No advertising support on this device", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopAdvertising();
+    }
+
+    private void startAdvertising() {
+        if (mBLEAdvertiser == null) return;
+        AdvertiseSettings settings = new AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+                .setConnectable(false)
+                .setTimeout(0)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+                .build();
+        AdvertiseData data = new AdvertiseData.Builder()
+                .addManufacturerData(BEACON_ID, buildGPSPacket())
+                .build();
+        mBLEAdvertiser.startAdvertising(settings, data, mAdvertiseCallback);
+    }
+
+    private void stopAdvertising() {
+        if (mBLEAdvertiser == null) return;
+        mBLEAdvertiser.stopAdvertising(mAdvertiseCallback);
+    }
+
+    private void restartAdvertising() {
+        stopAdvertising();
+        startAdvertising();
+    }
+
+    private AdvertiseCallback mAdvertiseCallback = new AdvertiseCallback() {
+        @Override
+        public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+            super.onStartSuccess(settingsInEffect);
+            String msg = "Service Running";
+            TextView tv1 = (TextView)findViewById(R.id.textView);
+            tv1.setText(msg);
+            mHandler.sendMessage(Message.obtain(null, 0, msg));
+        }
+
+        @Override
+        public void onStartFailure(int errorCode) {
+            if (errorCode != ADVERTISE_FAILED_ALREADY_STARTED) {
+                String msg = "Service failed to start: " + errorCode;
+                mHandler.sendMessage(Message.obtain(null, 0, msg));
+            } else {
+                restartAdvertising();
+            }
+        }
+    };
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+/*
+UI feedback to the user would go here.
+*/
+        }
+    };
+
+    private byte[] buildGPSPacket() {
+        byte[] packet = new byte[1];
+        packet[0] = 1;
+        /* GPS code packet generation goes here */
+        return packet;
+    }
+
+    public void buttonPress(View view){
+        startAdvertising();
     }
 }
