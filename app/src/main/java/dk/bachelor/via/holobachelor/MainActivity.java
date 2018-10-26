6cacd0d93d86c10b6519e6f4db4ac614655bccb5
Bogdan <ene.bogd@gmail.com>
2018-10-26 15:23:30 +0200

    Implemented rotation and fixed pinching

--- app/src/main/java/dk/bachelor/via/holobachelor/MainActivity.java
+++ app/src/main/java/dk/bachelor/via/holobachelor/MainActivity.java
@@ -8,6 +8,7 @@ import android.bluetooth.le.AdvertiseSettings;
 import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Debug;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -15,18 +16,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static java.lang.Thread.sleep;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements RotationGestureDetector.OnRotationGestureListener {
     BluetoothAdapter mBAdapter;
     BluetoothManager mBManager;
     BluetoothLeAdvertiser mBLEAdvertiser;
     static final int BEACON_ID = 1775;
     AdvertiseData data;
     private GestureDetector mDetector;
+    private ScaleGestureDetector mScaleGestureDetector;
+    private RotationGestureDetector mRotationDetector;
+    private float mScaleFactor = 1.0f;
+    private boolean screenIsTouched = false;
+    private float angle = 0.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
         // this is the view we will add the gesture detector to
         View myView = findViewById(R.id.gesture_view);
+        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+        mRotationDetector = new RotationGestureDetector(this);
 
         // get the gesture detector
         mDetector = new GestureDetector(this, new MyGestureListener());
@@ -84,6 +95,11 @@ public class MainActivity extends AppCompatActivity {
                 .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
                 .build();
         mBLEAdvertiser.startAdvertising(settings, data, mAdvertiseCallback);
+        try {
+            sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private void stopAdvertising() {
@@ -129,42 +145,128 @@ UI feedback to the user would go here.
         }
     };
 
-    private byte[] buildGPSPacket(byte payload) {
-        byte[] packet = new byte[1];
-        packet[0] = payload;
+    private byte[] buildGPSPacket(byte id, byte payload) {
+        byte[] packet = new byte[2];
+        packet[0] = id;
+        packet[1] = payload;
         return packet;
     }
 
-    private void createPacketWithData(byte payload) {
+    // IDs:
+    // 1 - Panning
+    // 2 - Zooming
+    // 3 - Rotating
+    private void createPacketWithData(byte id, byte payload) {
         data = new AdvertiseData.Builder()
-                .addManufacturerData(BEACON_ID, buildGPSPacket(payload))
+                .addManufacturerData(BEACON_ID, buildGPSPacket(id, payload))
                 .build();
         startAdvertising();
     }
 
     public void buttonPress(View view){
-        createPacketWithData((byte) 1);
+        createPacketWithData((byte) 1, (byte)1);
     }
 
     public void buttonPress2(View view){
-        createPacketWithData((byte) 2);
+        createPacketWithData((byte) 1, (byte) 2);
     }
 
     // Gesture Control
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        mScaleGestureDetector.onTouchEvent(motionEvent);
+        mRotationDetector.onTouchEvent(motionEvent);
+        // pass the events to the gesture detector
+        // a return value of true means the detector is handling it
+        // a return value of false means the detector didn't
+        // recognize the event
+        if (motionEvent.getAction() == android.view.MotionEvent.ACTION_DOWN) {
+            Log.d("TouchTest", "Touch down");
+            screenIsTouched = true;
+        } else if (motionEvent.getAction() == android.view.MotionEvent.ACTION_UP) {
+            Log.d("TouchTest", "Touch up");
+            // Log.d("RotationGestureDetector", "Rotation: " + Float.toString(angle));
+            // payload: 1 for positive, 0 for negative rotation
+            // positive rotation is counter clockwise
+            if(angle > 25) {
+                createPacketWithData((byte) 3, (byte) 1);
+                Log.d("RotationGestureDetector", "Positive Rotation");
+            }
+            else if(angle < -25) {
+                createPacketWithData((byte) 3, (byte) 0);
+                Log.d("RotationGestureDetector", "Negative Rotation");
+            }
+        }
+        return super.onTouchEvent(motionEvent);
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+
+        private float originalValue = 128;
+
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector){
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(1.0f,
+                    Math.min(mScaleFactor, 256.0f));
+            Log.d("TAG", "Scale factor: " + Float.toString(mScaleFactor));
+            return true;
+        }
+
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector detector){
+            Log.d("Scale", "It started");
+            mScaleFactor = originalValue;
+            return true;
+        }
+
+        @Override
+        public void onScaleEnd (ScaleGestureDetector detector){
+
+            if(mScaleFactor > originalValue && Math.abs(angle) < 25) {
+                // 1 for zoom in
+                createPacketWithData((byte) 2, (byte) 1);
+                Log.d("Scale", "Scale factor: " + Float.toString(mScaleFactor));
+                Log.d("Scale", "Zoomed in");
+            } else if (mScaleFactor < originalValue && Math.abs(angle) < 25){
+                // 0 for zoom out
+                createPacketWithData((byte) 2, (byte) 0);
+                Log.d("Scale", "Scale factor: " + Float.toString(mScaleFactor));
+                Log.d("Scale", "Zoomed out");
+            }
+            Log.d("Scale", "It ended");
+        }
+    }
+
     View.OnTouchListener touchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View v, MotionEvent event) {
-            // pass the events to the gesture detector
-            // a return value of true means the detector is handling it
-            // a return value of false means the detector didn't
-            // recognize the event
             return mDetector.onTouchEvent(event);
 
         }
     };
 
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+    @Override
+    public void OnRotation(RotationGestureDetector rotationDetector) {
+        angle = rotationDetector.getAngle();
+         Log.d("RotationGestureDetector", "Rotation: " + Float.toString(angle));
+        // payload: 1 for positive, 0 for negative rotation
+        // positive rotation is counter clockwise
+        /*
+        if(angle > 0 && !screenIsTouched) {
+            createPacketWithData((byte) 3, (byte) 1);
+            Log.d("RotationGestureDetector", "Positive Rotation");
+        }
+        else if(angle < 0 && !screenIsTouched) {
+            createPacketWithData((byte) 3, (byte) 0);
+            Log.d("RotationGestureDetector", "Negative Rotation");
+        }*/
 
+
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+/*
         @Override
         public boolean onDown(MotionEvent event) {
             Log.d("TAG","onDown: ");
@@ -203,7 +305,6 @@ UI feedback to the user would go here.
                                float velocityX, float velocityY) {
             Log.d("TAG", "onFling: ");
             return true;
-        }
+        }*/
     }
-
 }
