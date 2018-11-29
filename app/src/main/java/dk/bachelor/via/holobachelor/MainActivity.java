b221dcf98e438a385277cef3134355fc5977e42c
BenSolyom <solyombence@freemail.hu>
2018-11-29 13:10:34 +0100

    Fixed orientation change layout bugs

--- app/src/main/java/dk/bachelor/via/holobachelor/MainActivity.java
+++ app/src/main/java/dk/bachelor/via/holobachelor/MainActivity.java
@@ -5,10 +5,15 @@ import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.AdvertiseData;
 import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.content.Intent;
+import android.content.SharedPreferences.Editor;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.NonNull;
+
+
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -25,25 +30,41 @@ import org.w3c.dom.Text;
 import Broadcaster.Broadcaster;
 
 
-
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
     BluetoothAdapter mBAdapter;
     BluetoothManager mBManager;
     BluetoothLeAdvertiser mBLEAdvertiser;
     AdvertiseData data;
     public static Broadcaster broadcaster;
     public static Handler mainThreadHandler;
-
+    private Editor editor;
     String status;
+    SharedPreferences pref;
 
+    // executed only on startup
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        pref = getApplicationContext().getSharedPreferences("Pref", 0); // 0 - for private mode
+        editor = pref.edit();
+        executeLogic();
+    }
+
+    @Override
+    protected void onStop() {
+        editor.putString("selectedFragment", "Navigation");
+        editor.commit();
+        super.onStop();
+    }
+
+    // executed on orientation change
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        executeLogic();
+    }
+
+    public void executeLogic() {
         setContentView(R.layout.activity_main);
-
-
-        status = "";
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -52,14 +73,20 @@ public class MainActivity extends AppCompatActivity{
                 switch (item.getItemId()) {
                     case R.id.navigation_navigation:
                         selectedFragment = new NavigationFragment();
+                        editor.putString("selectedFragment", "Navigation");
+                        editor.commit();
                         break;
 
                     case R.id.navigation_settings:
                         selectedFragment = new SettingsFragment();
+                        editor.putString("selectedFragment", "Settings");
+                        editor.commit();
                         break;
 
                     case R.id.navigation_input:
                         selectedFragment = new InputFragment();
+                        editor.putString("selectedFragment", "Input");
+                        editor.commit();
                         break;
                 }
                 getSupportFragmentManager().beginTransaction().replace(R.id.frame, selectedFragment).commit();
@@ -67,21 +94,31 @@ public class MainActivity extends AppCompatActivity{
                 return true;
             }
         });
+        if (pref.getString("selectedFragment", "defaultValue").equals("Navigation") || pref.getString("selectedFragment", "defaultValue").equals("defaultValue")) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame, new NavigationFragment()).commit();
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame, new NavigationFragment()).commit();
+            navigation.getMenu().findItem(R.id.navigation_navigation).setChecked(true);
 
+            Menu menu = navigation.getMenu();
+            MenuItem menuItem = menu.getItem(1);
+            menuItem.setChecked(true);
+        } else if (pref.getString("selectedFragment", "defaultValue").equals("Settings")) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame, new SettingsFragment()).commit();
 
-        navigation.getMenu().findItem(R.id.navigation_navigation).setChecked(true);
+            navigation.getMenu().findItem(R.id.navigation_settings).setChecked(true);
 
-        Menu menu = navigation.getMenu();
-        MenuItem menuItem = menu.getItem(1);
-        menuItem.setChecked(true);
+            Menu menu = navigation.getMenu();
+            MenuItem menuItem = menu.getItem(2);
+            menuItem.setChecked(true);
+        } else {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame, new InputFragment()).commit();
 
-        // this is the view we will add the gesture detector to
+            navigation.getMenu().findItem(R.id.navigation_input).setChecked(true);
 
-        // Add a touch listener to the view
-        // The touch listener passes all its events on to the gesture detector
-
+            Menu menu = navigation.getMenu();
+            MenuItem menuItem = menu.getItem(0);
+            menuItem.setChecked(true);
+        }
         mainThreadHandler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
@@ -104,9 +141,6 @@ public class MainActivity extends AppCompatActivity{
                 broadcaster = Broadcaster.getInstance(mBManager, mBAdapter, mBLEAdvertiser, mainThreadHandler);
             }
         }
-
-
-
     }
 
     @Override
@@ -130,11 +164,11 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onPause() {
         super.onPause();
-        if(broadcaster != null)
+        if (broadcaster != null)
             broadcaster.stopAdvertising();
     }
 
-    public void panMap(View view){
+    public void panMap(View view) {
         /* first argument is the movement type
         second is the direction of panning, going CSS style
         1 - North
@@ -143,12 +177,12 @@ public class MainActivity extends AppCompatActivity{
         4 - West
          */
         byte[] data = {Byte.parseByte(view.getTag().toString())};
-        if(broadcaster != null)
+        if (broadcaster != null)
             passUserInput((byte) 1, data);
     }
 
-    public void passUserInput(byte type, byte[] info){
-        if(broadcaster != null)
+    public void passUserInput(byte type, byte[] info) {
+        if (broadcaster != null)
             broadcaster.createPacketWithData(type, info);
     }
 
